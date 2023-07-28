abebfb0447954de429970cb2eca3ed00e20beea3
ntmanua <manuarii.natanni@gmail.com>
2023-07-28 14:25:38 +0200

    config tailwindcss

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,38 @@
-import styles from './MovieList.module.css';
-
 const MovieList = ({ moviesData }) => {
     const showMovies = moviesData.map((movie) => {
         return (
-            <div className={styles.movieSheet}>
+            <article
+                key={movie.id}
+                className='overflow-hidden rounded-lg shadow transition hover:shadow-lg'
+            >
                 <img
-                    className={styles.poster}
-                    key={movie.id}
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     alt={movie.title}
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    className='h-56 w-full object-cover'
                 />
-                <h3>{movie.title}</h3>
-                <p>{movie.release_date}</p>
-                <p>{movie.overview}</p>
-            </div>
+
+                <div className='bg-white p-4 sm:p-6'>
+                    <time
+                        dateTime='2022-10-10'
+                        className='block text-xs text-gray-500'
+                    >
+                        {movie.release_date}
+                    </time>
+
+                    <a href='#'>
+                        <h3 className='mt-0.5 text-lg text-gray-900'>
+                            {movie.title}
+                        </h3>
+                    </a>
+
+                    <p className='mt-2 line-clamp-3 text-sm/relaxed text-gray-500'>
+                        {movie.overview}
+                    </p>
+                </div>
+            </article>
         );
     });
-    return <div className={styles.container}>{showMovies}</div>;
+    return <div>{showMovies}</div>;
 };
 
 export default MovieList;
