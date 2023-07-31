b4bc9dc4d698e2f0ad36163b5aa3a6e2b095ddbd
ntmanua <manuarii.natanni@gmail.com>
2023-07-31 11:04:20 +0200

    fetch movies by actor name OK

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react';
 import styles from './SearchSection.module.css';
 import SearchBar from './SearchBar/SearchBar';
 import MovieList from './MovieList/MovieList';
-import fetchMovies from '@/utils/api';
+import { fetchMovies, getActorId, fetchMoviesByActor } from '@/utils/api';
 
 const SearchSection = () => {
     const [moviesData, setMoviesData] = useState([]);
     const [term, setTerm] = useState('');
 
     const handleSubmit = async () => {
-        const res = await fetchMovies(term);
+        const res = await fetchMoviesByActor(term);
         setMoviesData(res);
         console.log(res);
     };
--- src/utils/api.js
+++ src/utils/api.js
@@ -7,11 +7,10 @@ const fetchMovies = async (term) => {
             {
                 params: {
                     api_key: '30bd5b107af7a883b3777693032f3fac',
-                    query: term,
+                    query: encodeURI(term),
                 },
             }
         );
-
         if (response.data) {
             return response.data.results;
         } else {
@@ -24,4 +23,51 @@ const fetchMovies = async (term) => {
     }
 };
 
-export default fetchMovies;
+const getActorId = async (term) => {
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/search/person?`,
+            {
+                params: {
+                    api_key: '30bd5b107af7a883b3777693032f3fac',
+                    query: encodeURI(term),
+                },
+            }
+        );
+        if (response.data) {
+            console.log(response.data.results[0].id);
+            return response.data.results[0].id;
+        } else {
+            // Handle the case where 'results' is not available in the response
+            console.log("No 'results' found in the response.");
+        }
+    } catch (error) {
+        // Handle the error if the request fails
+        console.error('Error fetching data:', error);
+    }
+};
+const fetchMoviesByActor = async (term) => {
+    try {
+        const actorId = await getActorId(term);
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/person/${actorId}/movie_credits?`,
+            {
+                params: {
+                    api_key: '30bd5b107af7a883b3777693032f3fac',
+                },
+            }
+        );
+        if (response.data) {
+            console.log(response.data.cast);
+            return response.data.cast;
+        } else {
+            // Handle the case where 'results' is not available in the response
+            console.log("No 'results' found in the response.");
+        }
+    } catch (error) {
+        // Handle the error if the request fails
+        console.error('Error fetching data:', error);
+    }
+};
+
+export { fetchMovies, getActorId, fetchMoviesByActor };
