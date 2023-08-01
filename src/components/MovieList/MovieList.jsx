68ae24362a77e98702b46158c635fca9b8b37240
ntmanua <manuarii.natanni@gmail.com>
2023-08-01 14:19:39 +0200

    display movies notes

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,12 +6,7 @@ const MovieList = ({ moviesData }) => {
         const day = movie.release_date.split('-')[2];
 
         return (
-            <article
-                key={movie.id}
-                className={`flex bg-white border-2 transition-all delay-${
-                    index + 1
-                } duration-500 hover:shadow-xl sm h-96`}
-            >
+            <article class='flex bg-white transition hover:shadow-xl border-2'>
                 <div className='rotate-180 p-2 [writing-mode:_vertical-lr]'>
                     <time
                         dateTime={movie.release_date}
@@ -35,15 +30,31 @@ const MovieList = ({ moviesData }) => {
                         }
                         className='h-auto w-full object-cover'
                     />
+                    <div class='flex items-center justify-center p-2'>
+                        <svg
+                            class='w-4 h-4 text-yellow-300 mr-1'
+                            aria-hidden='true'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='currentColor'
+                            viewBox='0 0 22 20'
+                        >
+                            <path d='M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z' />
+                        </svg>
+                        <p class='ml-2 text-xl font-bold text-gray-900 dark:text-black'>
+                            {movie.vote_average}
+                        </p>
+                        <span class='w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400'></span>
+                        <p class='text-xl font-medium text-gray-900 dark:text-black'>
+                            {movie.vote_count} vote
+                        </p>
+                    </div>
                 </div>
 
                 <div className='flex flex-1 flex-col justify-between overflow-hidden'>
                     <div className='h-full border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6'>
-                        <a href='#'>
-                            <h3 className='font-bold uppercase text-gray-900 text-[25px]'>
-                                {movie.title}
-                            </h3>
-                        </a>
+                        <h3 className='font-bold uppercase text-gray-900 text-[25px]'>
+                            {movie.title}
+                        </h3>
 
                         <p className='mt-2 text-sm/relaxed text-gray-700 text-[20px]'>
                             {movie.overview}
@@ -55,7 +66,7 @@ const MovieList = ({ moviesData }) => {
     });
 
     return (
-        <div className='h-full w-full p-4 grid grid-cols-1 gap-2 lg:grid-cols-3 lg:gap-10'>
+        <div className='h-full w-full p-4 grid grid-cols-1 gap-2 lg:grid-cols-3 lg:gap-5'>
             {showMovies}
         </div>
     );
