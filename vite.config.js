9dd66510a4ae44099a611745731b641bedb15e69
ntmanua <manuarii.natanni@gmail.com>
2023-08-02 16:08:50 +0200

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-    base: 'test-sport-react',
+    base: 'test-spot-react',
     plugins: [react()],
 });
