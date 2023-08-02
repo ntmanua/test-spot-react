2c1f847e4b18c33dd6b34523c534bd8a132b5414
ntmanua <manuarii.natanni@gmail.com>
2023-08-02 18:26:57 +0200

    rm custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            height: {
-                custom: 'calc(100% - 180px - 56px)',
-            },
-        },
-    },
     plugins: [],
 };
